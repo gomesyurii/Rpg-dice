2090ca27f25d3575a6f6137ec0c664669dbb10a1
Yuri de Jesus Gomes <dev.yurigomes@gmail.com>
2024-02-07 10:23:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(0x888888); 
+renderer.setClearColor(0x888888); // Define o fundo como cinza
 document.body.appendChild(renderer.domElement);
 
 
@@ -11,10 +12,10 @@ light.position.set(1, 1, 1).normalize();
 scene.add(light);
 
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const geometry = new THREE.IcosahedronGeometry(1, 0); 
 const material = new THREE.MeshPhongMaterial({ color: 0xadd8e6 }); 
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const d20 = new THREE.Mesh(geometry, material);
+scene.add(d20);
 
 camera.position.z = 5;
 
@@ -25,11 +26,8 @@ function onMouseMove(event) {
   const mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
 
 
-  cube.position.x = mouseX * 5;
-  cube.position.y = mouseY * 5;
-
-
-  renderer.render(scene, camera);
+  d20.position.x = mouseX * 5;
+  d20.position.y = mouseY * 5;
 }
 
 
@@ -38,8 +36,11 @@ document.addEventListener('mousemove', onMouseMove, false);
 
 function animate() {
   requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
+  d20.rotation.x += 0.01;
+  d20.rotation.y += 0.01;
+
+ 
+  renderer.render(scene, camera);
 }
 
 animate();
